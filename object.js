97a4277d25b7ca392f8c219253e8a35362ee26f4
WebCodersS <sadr0311@gmail.com>
2022-11-23 07:10:54 +0200

    first abject javascript

---
+++ object.js
@@ -0,0 +1,27 @@
+//  homework 2
+let person = {
+    MyName: "Sadriddin",
+    Surname: "Xolmatov",
+    age: 19,
+    Study: "TATU QF",
+    level: "3 kurs",
+    Number: "+998990021103",
+}
+
+console.log(person);
+
+// homework 3
+let prompt_1 = prompt("Bu yerga son kiriting");
+let prompt_2 = prompt("Bu yerga keyingi son kiriting");
+console.log(Number(prompt_1) % Number(prompt_2));
+
+
+
+// homework 1 
+
+let MyName = "Sadriddin";
+let lastname = "Xolmatov";
+let age = 19;
+let Study = "TATU Qarshi felialida";
+let level = "3-kurs talabasiman";
+console.log(`Meni ismim ${MyName}, Familiyam bu ${lastname}, Hozir uqiyapman ${Study} ${level}`);
